e305526d6f65a309bd2ec423812440ff025fb330
ytochkaa <darya.kolupaeva5654@mail.ru>
2023-12-09 15:30:48 +0700

    add to CreateEncodeFile()

--- Haffman.cpp
+++ Haffman.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <map>
-
+#include <list>
+#include <fstream>
 
 using namespace std;
 
@@ -46,8 +47,65 @@ void Table(Node *root) {
     bitcode.pop_back();
 }
 
+void CreateEncodedFile() {
+    ifstream f("text.txt", ios::binary);
+    ofstream encodedFile("encoded.txt", ios::binary);
 
-int main(){
+    int count = 0;
+    char buf = 0;
+    while (!f.eof()) {
+        char c = f.get();
+        vector<bool> x = mappingtable[c];
+        for (int n = 0; n < x.size(); n++) {
+            buf = buf | x[n] << (7 - count);
+            count++;
+            if (count == 8) {
+                count = 0;
+                encodedFile.write((char*)& buf, sizeof(buf));
+                buf = 0;
+            }
+        }
+    }
+
+    f.close();
+    encodedFile.close();
+}
+
+
+
+int main(int argc, char *argv[]){
+    ifstream f("text.txt", ios::binary);
+    map <char, int> m;
+
+    while (!f.eof()){
+        char c = f.get();
+        m[c]++;
+    }
+
+    list<Node *> t;
+    for (map<char, int>::iterator itr = m.begin(); itr != m.end(); ++itr){
+        Node *p = new Node;
+        p->c = itr->first;
+        p->a = itr->second;
+        t.push_back(p);
+    }
+
+    while (t.size() != 1){
+        t.sort(MyCompare());
+
+        Node *Lchild = t.front();
+        t.pop_front();
+        Node *Rchild = t.front();
+        t.pop_front();
+
+        Node *parent = new Node(Lchild, Rchild);
+        t.push_back(parent);
+    }
+
+    Node *root = t.front();
+
+    Table(root);
+    CreateEncodedFile();
 
     return 0;
 }
